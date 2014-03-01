0712bba0f9706bf83fb45a981b1b4e174e0f7184
arminakvn <armin.akhavan@gmail.com>
2014-03-01 17:28:24 -0500

    sidebar module created. todo: create the header module based on the footer example only with the show submodule

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,11 @@
 //= require_tree ./backbone/entities
 //= require_tree ./backbone/controllers
 //= require_tree ./backbone/views
+
+//= require_tree ./backbone/config
+//= require backbone/app
+//= require_tree ./backbone/controllers
+//= require_tree ./backbone/entities
+//= require_tree ./backbone/views
+//= require_tree ./backbone/components
+//= require_tree ./backbone/apps
---
+++ app/assets/javascripts/backbone/apps/sidebar/list/list_controller.js.coffee
@@ -0,0 +1,13 @@
+@Equitabletod.module "SidebarApp.List", (List, App, Backbone, Marionette, $, _) ->
+
+  class List.Controller extends App.Controllers.Application
+
+    initialize: ->
+      @layout = @getLayoutView()
+
+      @listenTo @layout, "show", =>
+
+      @show @layout
+
+    getLayoutView: ->
+      new List.Layout
