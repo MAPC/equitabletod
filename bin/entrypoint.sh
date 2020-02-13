ea34353f250ab3ca797f1ca8de080c465c5c4abe
Matthew Zagaja <mzagaja@mapc.org>
2020-02-13 09:42:42 -0500

    Remove Postgres Check
    Since Heroku does not use docker-compose.yml and is just running the web Dockerfile, we do not want to check for postgres using these credentials. For now we comment out this code to potentially be updated using more abstract postgres login credentials for this check.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-until psql -h "db" -U "postgres" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+# until psql -h "db" -U "postgres" -c '\q'; do
+#   >&2 echo "Postgres is unavailable - sleeping"
+#   sleep 1
+# done
 
->&2 echo "Postgres is up - executing command"
+# >&2 echo "Postgres is up - executing command"
 exec "$@"
